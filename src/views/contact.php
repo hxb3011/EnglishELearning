0a6481bdcafa8cc709cd28e66fb5d3b364676863
NKaii404 <nnhatkhai83@gmail.com>
2024-03-06 16:56:41 +0700

    create contact

---
+++ src/views/contact.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <title>Home Page</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Exo%3A600%2C700" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A400%2C500%2C600%2C700" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost%3A400%2C500" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../clients/css/home/home_main.css">
+</head>
+
+<body>
+
+    <div class="header">
+        <nav class="navbar navbar-expand-lg  header__navbar">
+            <div class="container">
+                <div class="header__navbar__brand-wrapper">
+                    <a class="navbar-brand header__navbar-brand" href="#">
+                        <img src="../assets/images/icon.png" class="header__navbar-logo" alt="Logo">
+                        ELearning
+                    </a>
+                </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse header__navbar-menu-wrapper" id="navbarSupportedContent">
+                    <ul class="navbar-nav mb-2 mb-lg-0 header__navbar-menu">
+                        <li class="nav-item header__navbar-menu__item">
+                            <a class="nav-link" aria-current="page" href="#">Trang chủ</a>
+                        </li>
+                        <li class="nav-item header__navbar-menu__item">
+                            <a class="nav-link " href="#">Khóa học</a>
+                        </li>
+                        <li class="nav-item header__navbar-menu__item">
+                            <a class="nav-link" href="#">Blog</a>
+                        </li>
+                        <li class="nav-item header__navbar-menu__item">
+                            <a class="nav-link" href="#">FAQs</a>
+                        </li>
+                        <li class="nav-item header__navbar-menu__item active">
+                            <a class="nav-link" href="#">Liên hệ</a>
+                        </li>
+                    </ul>
+                    <div class="d-flex header__auth justify-content-end align-items-center">
+                        <a href="#" class="header__auth-link">
+                            Login/Register
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="d-flex">
+                        <a class="navbar-brand header__navbar-brand" href="#">
+                            <img src="../assets/images/icon.png" class="header__navbar-logo" alt="Logo">
+                            ELearning
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3 d-flex justify-content-center align-items-center">
+                    <div class="footer-heading ">
+                        Cần trợ giúp ?
+                    </div>
+                </div>
+                <div class="col-md-3 d-flex justify-content-center align-items-center">
+                    <div class="footer-heading ">
+                        Khóa học
+                    </div>
+                </div>
+                <div class="col-md-3 d-flex justify-content-center align-items-center">
+                    <div class="footer-heading ">
+                        Liên hệ
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <p class="footer_greeting ">
+                        Chào mừng đến với trang web học tiếng Anh trực tuyến! Chúng tôi cung cấp một nền tảng học tập linh hoạt và tiện lợi cho mọi người muốn nâng cao kỹ năng tiếng Anh của mình. Với hàng trăm bài học, bài kiểm tra, và tài nguyên học liệu đa dạng, chúng tôi cam kết mang đến trải nghiệm học tập tích cực và hiệu quả nhất cho bạn."
+                    </p>
+                </div>
+                <div class="col-md-3 d-flex justify-content-center">
+                    <ul class="footer__list">
+                        <li class="footer__list-item">
+                            <a href="#" class="footer__list-item__link">
+                                Liên hệ
+                            </a>
+                        </li>
+                        <li class="footer__list-item">
+                            <a href="#" class="footer__list-item__link">
+                                Bài viết mới
+                            </a>
+                        </li>
+                        <li class="footer__list-item">
+                            <a href="#" class="footer__list-item__link">
+                                FAQ
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-3 d-flex justify-content-center">
+                    <ul class="footer__list">
+                        <li class="footer__list-item">
+                            <a href="#" class="footer__list-item__link">
+                                Các khóa học
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <p class="footer_text">Address: 2321 New Design Str, Lorem Ipsum10 Hudson Yards, USA</p>
+                    <p class="footer_text"><img src="" class="footer-icon" alt="">Tel:+ (123) 2500-567-8988</p>
+                    <p class="footer_text"><img src="" class="footer-icon" alt="">Phone:+ (123) 2500-567-8988</p>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
