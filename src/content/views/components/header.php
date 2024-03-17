5b26f99ca7e082ad59e6abb62adb693a25fe1db0
PhongKP <koongchanphong0712@gmail.com>
2024-03-17 19:45:55 +0700

    test add config routing

--- src/content/views/components/header.php
+++ src/content/views/components/header.php
@@ -3,16 +3,15 @@
 
 <head>
     <meta charset="utf-8" />
-    <link rel="icon" href="/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#000000" />
     <title><?= isset($title) ? $title : 'Hệ thống E-learning' ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../../public/css/home/home_main.css">
-    <link rel="icon" type="image/x-icon" href="../../public/images/logo-icon.png">
-    <link rel="stylesheet" href="../../public/css/header/header.css">
+    <link rel="stylesheet" href="/englishelearning/src/public/css/home/home_main.css">
+    <link rel="icon" type="image/x-icon" href="/englishelearning/src/public/images/logo-icon.png">
+    <link rel="stylesheet" href="/englishelearning/src/public/css/header/header.css">
 </head>
 
 <body>
@@ -22,7 +21,7 @@
             <div class="container">
                 <div class="header__navbar__brand-wrapper">
                     <a class="navbar-brand header__navbar-brand" href="#">
-                        <img src="../../public/images/icon.png" class="header__navbar-logo" alt="Logo">
+                        <img src="/englishelearning/src/public/images/icon.png" class="header__navbar-logo" alt="Logo">
                         ELearning
                     </a>
                 </div>
@@ -70,12 +69,12 @@
             <div class="container">
                 <?php
                     echo "<ul>";
-                    echo "<li><a href='../home.php' class='nav-link'>Home </a></li>";
+                    echo "<li><a href='/englishelearning/src/content/views/home.php' class='nav-link'>Home </a></li>";
                     if ($currentPage != 'home'){
                         echo " > ";
                     }
                     if ($currentPage == 'bloglist'){
-                        echo "<li><a href='../bloglist.php' class='nav-link'>Blog </a></li>";
+                        echo "<li><a href='/englishelearning/src/content/views/bloglist.php' class='nav-link'>Blog </a></li>";
                     }
                 ?>
             </div>
