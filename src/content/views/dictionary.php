8b30451d2a5ca63e65104f3ec5c4265e548458e5
NKaii404 <nnhatkhai83@gmail.com>
2024-03-21 17:45:40 +0700

    Test_Dictionary_layout

--- src/content/views/dictionary.php
+++ src/content/views/dictionary.php
@@ -3,22 +3,57 @@ $title = "Dictionary - Hệ thống học tiếng anh Vocala";
 require('./components/header.php') 
 ?>
 <link rel="stylesheet" href="../../public/css/dictionary/dictionary.css">
-<div class="container mt-5 mb-5">
-    <div class="section-heading section-heading--lg mb-5 pb-5">Dictionary</div>
-    <div class = "container-fluid flashcard__container">
-        <div class="row mt-5">
-            <div class="col-md-3 ">
+<div class="container mt-5 mb-5" >
+    <div class="section-heading section-heading--lg ">Dictionary</div>
+    <div class = "container flashcard__container">
+        <div class="row mt-5 justify-content-center">
+            <div class="col-xs-8 col-sm-6 col-md-6 col-lg-3 my-3">
                 <div class="card text-bg-dark text-light rounded-5 p-5">
-                    <h3 class="card-header text-reset">Noun</h3>
+                    <h3 class="pretitle text-end ">Noun</h3>
                     <img src="../../public/images/icon-blog.png" class="feedback-icon" alt="">
-                    <h3 class="card-title feedback-own text-reset">Word</h3>
-                    <p class="card-text feedback-own_role text-reset">Học viên</p>
-                    <p class="feedback-content text-reset" align ="jusitify">Tôi phải giải thích cho bạn biết tại sao tất cả điều này lại sai lầm. Tdea 
-                    tố cáo niềm vui và ca ngợi nỗi đau đã ra đời và tôi sẽ cung cấp cho bạn một tài khoản đầy đủ về hệ thống và giải thích</p>
+                    <h3 class="card-title word_title text-reset">Coffee</h3>
+                    <p class="word_pronouce  text-reset opacity-75 ">/ˈkɒfi/</p>
+                    <p class="word_definition text-reset" align ="jusitify"> the roasted seeds (called coffee beans) of a tropical bush; a powder made from them</p>
+                </div>
+            </div>
+            <div class="col-xs-8 col-sm-6 col-md-6 col-lg-3 my-3">
+                <div class="card text-bg-dark text-light rounded-5 p-5">
+                    <h3 class="pretitle text-end ">Noun</h3>
+                    <img src="../../public/images/icon-blog.png" class="feedback-icon" alt="">
+                    <h3 class="card-title word_title text-reset">Coffee</h3>
+                    <p class="word_pronouce  text-reset opacity-75 ">/ˈkɒfi/</p>
+                    <p class="word_definition text-reset" align ="jusitify"> the roasted seeds (called coffee beans) of a tropical bush; a powder made from them</p>
+                </div>
+            </div>
+            <div class="col-xs-8 col-sm-6 col-md-6 col-lg-3 my-3">
+                <div class="card text-bg-dark text-light rounded-5 p-5">
+                    <h3 class="pretitle text-end ">Noun</h3>
+                    <img src="../../public/images/icon-blog.png" class="feedback-icon" alt="">
+                    <h3 class="card-title word_title text-reset">Coffee</h3>
+                    <p class="word_pronouce  text-reset opacity-75 ">/ˈkɒfi/</p>
+                    <p class="word_definition text-reset" align ="jusitify"> the roasted seeds (called coffee beans) of a tropical bush; a powder made from them</p>
+                </div>
+            </div>
+            <div class="col-xs-8 col-sm-6 col-md-6 col-lg-3 my-3">
+                <div class="card text-bg-dark text-light rounded-5  p-5">
+                    <h3 class="pretitle text-end ">Noun</h3>
+                    <img src="../../public/images/icon-blog.png" class="feedback-icon" alt="">
+                    <h3 class="card-title word_title text-reset">coffee</h3>
+                    <p class="word_pronouce  text-reset opacity-75 ">/ˈkɒfi/</p>
+                    <p class="word_definition  text-reset " align ="jusitify"> the roasted seeds (called coffee beans) of a tropical bush; a powder made from them</p>
+                </div>
+            </div>
+            <div class="col-xs-8 col-sm-6 col-md-6 col-lg-3 my-3">
+                <div class="card text-bg-dark text-light rounded-5 p-5">
+                    <h3 class="pretitle text-end ">Adverb</h3>
+                    <img src="../../public/images/icon-blog.png" class="feedback-icon" alt="">
+                    <h3 class="card-title word_title text-reset">exponentially</h3>
+                    <p class="word_pronouce  text-reset opacity-75 "> /ˌek.spoʊˈnen.ʃəl.i/</p>
+                    <p class="word_definition text-reset" align ="jusitify"> in a way that becomes quicker and quicker as something that increases becomes larger</p>
                 </div>
             </div>
         </div>
-    </div> 
+    </div>
 </div>
 <?php
 require('./components/footer.php') ?>
