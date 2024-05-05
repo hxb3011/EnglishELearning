154ab9048c7a5bb3f4457f266cea983e1e1b2ddf
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-05-05 15:15:00 +0700

    sua lai file docker-compose

--- src/_lib/services/S3Service.php
+++ src/_lib/services/S3Service.php
@@ -1,7 +1,6 @@
 <?
 require_once "/var/www/html/_lib/utils/requir.php";
-requirl("vendor/aws/aws-autoloader.php");
-
+requirl('/composer/vendor/autoload.php');
 use Aws\S3\S3Client;
 use Aws\S3\Exception\S3Exception;
 class S3Service
