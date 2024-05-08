ba7a1ef13e0b969d6c4710fbedd22be786e8b766
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2024-05-08 20:43:36 +0700

    them cac tab cho sidebar admin

--- src/_adminLayout.php
+++ src/_adminLayout.php
@@ -79,6 +79,24 @@ $page->beforeDocument();
                         </li>
                     </ul>
                 </li>
+                <li class="sidebar-item">
+                    <a href="/administration/dashboard.php" class="sidebar-link" >
+                        <span class="mdi-b -account"></span>
+                        <span class="sidebar-item__text">Tài khoản</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="/administration/dashboard.php" class="sidebar-link" >
+                        <span class="mdi-b -user"></span>
+                        <span class="sidebar-item__text">User</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="/administration/dashboard.php" class="sidebar-link" >
+                        <span class="mdi-b -role"></span>
+                        <span class="sidebar-item__text">Role</span>
+                    </a>
+                </li>
             </ul>
             <div class="sidebar-footer">
                 <a href="/" class="sidebar-link">
