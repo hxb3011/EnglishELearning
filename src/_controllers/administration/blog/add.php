a2998e4fa5d9be5e84012aa987751852bdb4e280
Male29003 <nam2309200312345@gmail.com>
2024-05-13 13:24:36 +0700

    Them file add.php cho phan blog admin

--- src/_controllers/administration/blog/add.php
+++ src/_controllers/administration/blog/add.php
@@ -3,5 +3,9 @@ require_once "www/var/html/_lib_utils/requir.php" ;
 requirl("oopControllers/admin/blog.php");
 requirm("/access/Blog.php");
 $ctrl = new AdminBlog();
-
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //$ctrl->add_post();
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    //$ctrl->add();
+}
 ?>
